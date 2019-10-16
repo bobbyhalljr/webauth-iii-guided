6190f144c9b81ccd3c81af1e62bfb14a0fc6f377
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-16 13:21:09 -0400

    first part

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // generate token
+        const token = generateToken(user)
+        // add token to response
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`, 
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,18 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+// Generate Token
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '7d'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
